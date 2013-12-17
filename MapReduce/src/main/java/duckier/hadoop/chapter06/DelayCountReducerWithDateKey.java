1b147a160a81b44322aaf9521642de1119a49302
duckier <duckierS2@gmail.com>
2013-12-18 00:14:01 +0900

    주석 삭제

--- MapReduce/src/main/java/duckier/hadoop/chapter06/DelayCountReducerWithDateKey.java
+++ MapReduce/src/main/java/duckier/hadoop/chapter06/DelayCountReducerWithDateKey.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
-import org.mortbay.log.Log;
 
 public class DelayCountReducerWithDateKey extends
 		Reducer<DateKey, IntWritable, DateKey, IntWritable> {
@@ -30,26 +29,22 @@ public class DelayCountReducerWithDateKey extends
 		String[] columns = key.getYear().split(",");
 
 		int sum = 0;
-		// TODO 전역변수가 아닌데 이전키에 대한 값을 유지하는가??
 		Integer bMonth = key.getMonth();
 
 		if (columns[0].equals("D")) {
 			for (IntWritable value : values) {
 				if (bMonth != key.getMonth()) {
-					outputKey.setYear(columns[1]);
+					outputKey.setYear(key.getYear().substring(2));
 					outputKey.setMonth(bMonth);
 					result.set(sum);
 					mos.write("departure", outputKey, result);
 					sum = 0;
 				}
 				sum += value.get();
-				// TODO 다시 변수에 값을 셋팅하는 이유는?
 				bMonth = key.getMonth();
 			}
-			// TODO 마지막 월의 지연 횟수
 			if (bMonth == key.getMonth()) {
-				outputKey.setYear(columns[1]);
-				// TODO bMonth가 아닌 key.getMonth()를 하는 이유
+				outputKey.setYear(key.getYear().substring(2));
 				outputKey.setMonth(key.getMonth());
 				result.set(sum);
 				mos.write("departure", outputKey, result);
@@ -57,7 +52,7 @@ public class DelayCountReducerWithDateKey extends
 		} else {
 			for (IntWritable value : values) {
 				if (bMonth != key.getMonth()) {
-					outputKey.setYear(columns[1]);
+					outputKey.setYear(key.getYear().substring(2));
 					outputKey.setMonth(bMonth);
 					result.set(sum);
 					mos.write("arrival", outputKey, result);
@@ -67,7 +62,7 @@ public class DelayCountReducerWithDateKey extends
 				bMonth = key.getMonth();
 			}
 			if (bMonth == key.getMonth()) {
-				outputKey.setYear(columns[1]);
+				outputKey.setYear(key.getYear().substring(2));
 				outputKey.setMonth(key.getMonth());
 				result.set(sum);
 				mos.write("arrival", outputKey, result);
